d9b0baed4deef647c6b3e4d1f11945f08570228b
mj6meron <meronmkl@gmail.com>
2023-03-02 10:26:31 +0100

    some changes

--- api/get.js
+++ api/get.js
@@ -1,44 +1,39 @@
 const express = require("express");
 const router = express.Router();
 
-
-
 // Define routes for /, /door, /window, and /lamp APIs
-router.get("/", function (req, res) {
-  console.log("Request received at /:");
-  if (!req) {
-    res.send("You succefully send a request and request body: ", req);
-  } else {
+function getRoutes(io) {
+  router.get("/", function (req, res) {
+    console.log("Request received at /:");
     res.send(`This is the root API.`);
-  }
-});
+  });
 
-router.get("/door", function (req, res) {
-  console.log("Request received at /door:");
-  if (!req) {
-    res.send("You succefully send a request and request body: ", req);
-  } else {
+  router.get("/door", function (req, res) {
+    console.log("Request received at /door:");
     res.send(`This is the door API.`);
-  }
-});
+  });
 
-router.get("/window", function (req, res) {
-  console.log("Request received at /window:");
-  if (!req) {
-    res.send("You succefully send a request and request body: ", req);
-  } else {
+  router.get("/window", function (req, res) {
+    console.log("Request received at /window:");
     res.send(`This is the window API.`);
-  }
-});
+  });
 
-router.get("/lamp", function (req, res) {
-  console.log("Request received at /lamp:");
-  if (!req) {
-    res.send("You succefully send a request and request body: ", req);
-  } else {
-    res.send(`This is the lamp API.`);
-  }
-});
+  router.get("/lamp/on", function (req, res) {
+    console.log("Request received at /lamp: on");
+    // Send a message to the Socket.io server to turn the lamp on
+    io.emit('lamp:on', { data: 'on' });
+    res.send("Turning on lamp...");
+  });
 
+  router.get("/lamp/off", function (req, res) {
+    console.log("Request received at /lamp: off");
+    // Send a message to the Socket.io server to turn the lamp off
+    io.emit('lamp:off', { data: 'off' });
+    res.send("Turning off lamp...");
+  });
 
-module.exports = router;
+  return router;
+}
+
+module.exports = getRoutes;
+
--- server.js
+++ server.js
@@ -1,24 +1,64 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const http = require("http");
+const socketio = require("socket.io");
+const getRouter = require("./api/get");
 
 const app = express();
+
+const connectDatabase = require("./db/controllers/connectDB");
+// Connect to mongoDB database
+connectDatabase();
+
+// Serve static files from the "client" directory
+app.use(express.static("client"));
 
 // Use body-parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors()); // Use this after the variable declaration
 
-// Connect to MongoDB using Mongoose
-// This will be implemeted shortly
+// Create HTTP server and attach Socket.io
+const server = http.createServer(app);
+const io = socketio(server);
 
-// Use the GET and POST routers
-const getRouter = require("./api/get");
-const postRouter = require("./api/post");
-app.use("/", getRouter);
-app.use("/", postRouter);
+// Keep track of the current state of the lamp
+let lampState = "off";
+
+// Handle socket connection
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Send the current state of the lamp to the new client
+  socket.emit("lamp:status", { status: lampState });
+
+  // Handle incoming messages from the client
+  socket.on("lamp:on", (data) => {
+    console.log("Received lamp:on message");
+    // Update the state of the lamp and broadcast the new state to all connected clients
+    lampState = "on";
+    io.emit("lamp:status", { status: lampState });
+  });
+
+  socket.on("lamp:off", (data) => {
+    console.log("Received lamp:off message");
+    // Update the state of the lamp and broadcast the new state to all connected clients
+    lampState = "off";
+    io.emit("lamp:status", { status: lampState });
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+// Use the GET router
+app.use("/", getRouter(io));
 
 // Start the server
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+server.listen(port, function () {
   console.log(`Server is listening on port ${port}`);
 });
